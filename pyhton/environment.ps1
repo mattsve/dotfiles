40d0456819d9ab3226ff104d07d22ea2834e65d4
Mattias Svensson <mattsve@users.noreply.github.com>
2021-05-03 15:35:51 +0200

    Fix python command

--- pyhton/environment.ps1
+++ pyhton/environment.ps1
@@ -1,4 +1,4 @@
 if (Get-Command "python3" -ErrorAction SilentlyContinue) {
-    $scriptLocation = python -c "import os, sysconfig; print(sysconfig.get_path('scripts', f'{os.name}_user'))"
+    $scriptLocation = python3 -c "import os, sysconfig; print(sysconfig.get_path('scripts', f'{os.name}_user'))"
     $env:Path += ";$scriptLocation"
 }
