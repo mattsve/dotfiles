b657c3fc773fc8886aa766fba6dec13656b091e8
Mattias Svensson <mattsve@users.noreply.github.com>
2023-02-22 21:39:41 +0100

    Install thefuck if not installed

--- pyhton/environment.ps1
+++ pyhton/environment.ps1
@@ -2,4 +2,8 @@ $env:PYTHONIOENCODING='utf-8'
 if (Get-Command "python3" -ErrorAction SilentlyContinue) {
     $scriptLocation = python3 -c "import os, sysconfig; print(sysconfig.get_path('scripts', f'{os.name}_user'))"
     $env:Path += ";$scriptLocation"
+    
+    if (-Not (Get-Command "thefuck" -ErrorAction SilentlyContinue)) {
+        & pip install thefuck
+    }
 }
