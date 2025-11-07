62dd9aa898778b43b6653598ae8c7443e94d536c
Mattias Svensson <190869+mattsve@users.noreply.github.com>
2025-11-07 13:50:45 +0100

    Add embryo for darwin

---
+++ darwin/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -u
+
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo >> ~/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+fi
+
+if [[ ! -d "${HOME}/.dotfiles/" ]]; then
+    /opt/homebrew/bin/brew install —cask 1password
+    echo "Set upp 1password and enable ssh agent"
+    read -p "Press enter to continue"
+    git clone git@github.com:mattsve/dotfiles.git ~/.dotfiles 
+
+    echo "Run this command to add Homebrew to your PATH:"
+    echo '    eval "$(/opt/homebrew/bin/brew shellenv)"'
+    echo "Run this command to bootstrap the machine"
+    echo '    ~/.dotfiles/darwin/install.sh'
+    exit 0;
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+brew bundle install --file "${SCRIPT_DIR}/Brewfile"
