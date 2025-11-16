d98ac8c8369245b112e6da92b2f6664714d4df8c
Mattias Svensson <190869+mattsve@users.noreply.github.com>
2025-11-16 15:44:50 +0100

    Copy settings from 1password

--- applications/install.sh
+++ applications/install.sh
@@ -8,3 +8,18 @@ for dir in $SCRIPT_DIR/*/; do
     echo "  - $basename"
     "$dir/install.sh"
 done
+
+echo "Settings from 1password"
+op item list --vault Settings --format=json | jq -c '.[]' | while read -r item; do
+    echo "  - $(echo "$item" | jq -r '.title')"
+    details=$(op item get "$(echo "$item" | jq -r '.id')" --vault "$(echo "$item" | jq -r '.vault.id')" --format=json)
+    dir="$(echo "$details" | jq -r '.fields[] | select(.label == "path") | .value')"
+    dir="${dir/#\~/$HOME}" 
+    mkdir -p "${dir}"
+    echo "$details" | jq -c '.files[]' | while read -r file; do
+        path="$dir/$(echo "$file" | jq -r '.name')"
+        if [[ ! -f "${path}" ]]; then
+            op read "op://$(echo "$details" | jq -r '.vault.id')/$(echo "$details" | jq -r '.id')/$(echo "$file " | jq -r '.id')" > "${path}"
+        fi
+    done
+done
