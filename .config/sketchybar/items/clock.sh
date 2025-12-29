258eda89f118a2a85215168fadd963b34dc9b938
Mattias Svensson <190869+mattsve@users.noreply.github.com>
2025-12-30 00:06:20 +0100

    sketchybar tweaks

--- .config/sketchybar/items/clock.sh
+++ .config/sketchybar/items/clock.sh
@@ -2,5 +2,5 @@
 
 items+=(
     --add item clock $POSITION
-    --set clock update_freq=10 script="$PLUGIN_DIR/clock.sh"
+    --set clock update_freq=10 icon.drawing=off script="$PLUGIN_DIR/clock.sh"
 )
--- .config/sketchybar/items/focus.sh
+++ .config/sketchybar/items/focus.sh
@@ -2,7 +2,7 @@
 
 items+=(
     --add item focus $POSITION
-    --set focus script="$PLUGIN_DIR/focus.sh"
+    --set focus label.drawing=off script="$PLUGIN_DIR/focus.sh"
     --add event focus_on "_NSDoNotDisturbEnabledNotification"
 	--add event focus_off "_NSDoNotDisturbDisabledNotification"
     --subscribe focus focus_on focus_off
